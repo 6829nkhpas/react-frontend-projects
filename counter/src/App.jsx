7df4e92ffea86ae267e43685a8a353471830b58e
Naman Kumar <parnamnkh1@gmail.com>
2025-07-20 23:58:47 +0530

    Integrate Tailwind CSS into the project by adding dependencies and updating configuration. Refactor App component to utilize Tailwind for styling and remove unused CSS. Update package.json and package-lock.json accordingly.

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -1,22 +1,13 @@
-import { useState } from 'react'
+
 
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-const addValue = () =>{
-  setCount(count+1)
-}
-const removeValue = () =>{
-  setCount(count-1)
-}
+
+
   return (
     <>
-      <h1> Current value {count} </h1>
-      <h1>Prev Value {count -1 }</h1>
-      <button onClick={addValue}>Add Value</button>{ " "}
-      <button onClick={removeValue}>Remove Value Value</button>
-   
+     <h1 className='text-3xl font-bold bg-red-500 text-white'>Hello World</h1>
     </>
   )
 }
