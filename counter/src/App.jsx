07b9b2aca36a7648e63d54426d6aacdc0ac61b76
Naman Kumar <parnamnkh1@gmail.com>
2025-07-21 02:44:19 +0530

    Implement color-changing buttons in App component and remove unused CSS styles. The App now allows users to change the background color dynamically using Tailwind CSS for styling.

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -1,14 +1,24 @@
-
+import { useState } from 'react'
 
 import './App.css'
 
 function App() {
+  const [color, setColor] = useState("olive")
+
 
 
   return (
-    <>
-     <h1 className='text-3xl font-bold bg-red-500 text-white'>Hello World</h1>
-    </>
+    <div className='w-full h-screen duration-200' style={{backgroundColor: color, height: "100vh"}}>
+      <div className='fixed flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+        <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
+            <button onClick={() => {console.log("clicked"); setColor('red');} } className='outline-none text-black shadow-lg px-4 py-1 rounded-full'>red</button>
+            <button onClick={() => {console.log("clicked"); setColor('green');} } className='outline-none text-black shadow-lg px-4 py-1 rounded-full'>green</button>
+            <button onClick={() => {console.log("clicked"); setColor('pink');} } className='outline-none text-black shadow-lg px-4 py-1 rounded-full'>pink</button>
+
+
+        </div>
+      </div>
+    </div>
   )
 }
 
