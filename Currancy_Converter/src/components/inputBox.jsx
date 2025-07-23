20334127c0e735085fffc95a4e1d4cf70c2cc264
Naman Kumar <parnamnkh1@gmail.com>
2025-07-24 03:39:16 +0530

    Revamp App component layout by removing unused code and implementing a new background design for the Currency Converter. Update UI to enhance visual appeal and user engagement.

---
+++ Currancy_Converter/src/components/inputBox.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function inputBox({
+    label,
+    amount,
+    onAmountChange,
+    onCurrencyChange,
+    currencyOptions = [],
+    selectCurrency = "usd",
+    amountDisable = false,
+    currencyDisable = false,
+}) {
+  return (
+    <div>inputBox</div>
+  )
+}
+
+export default inputBox
