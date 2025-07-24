ac67fb9d8ddf15ace206d703b3ef8142d42643eb
Naman Kumar <parnamnkh1@gmail.com>
2025-07-24 23:53:19 +0530

    Enhance Currency Converter UI by updating the background image and text color in the App component. Introduce inputBox component for user input with improved layout and styling, including currency selection and amount input fields.

--- Currancy_Converter/src/App.jsx
+++ Currancy_Converter/src/App.jsx
@@ -7,8 +7,8 @@ function App() {
 
   return (
    
-    <div className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat" style ={{ backgroundImage: 'URL(https://images.pexels.com/photos/159888/pexels-photo-159888.jpeg)'}}>
-      <h1 className="text-4xl text-red-600 font-bold mb-8">Currency Converter</h1>
+    <div className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat" style ={{ backgroundImage: 'URL(https://images.pexels.com/photos/12955791/pexels-photo-12955791.jpeg)'}}>
+      <h1 className="text-4xl text-blue-400 bg-white font-bold mb-8">Currency Converter</h1>
     </div>
   )
 }
