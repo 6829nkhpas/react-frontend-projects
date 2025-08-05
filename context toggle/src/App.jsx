e6cf0db60902bf8e6833cc7b6b709333e526e598
Naman Kumar <parnamnkh1@gmail.com>
2025-08-06 03:05:34 +0530

    Implement theme context and toggle functionality in App component, enhancing user experience with light and dark mode support.

--- context toggle/src/App.jsx
+++ context toggle/src/App.jsx
@@ -1,14 +1,44 @@
 import { useState } from 'react'
-
 import './App.css'
+import Card from './components/cards.jsx'
+import ThemeBtn from './components/ThemeBtn.jsx'
+import { ThemeContext } from './contexts/theme.js'
+import { useEffect } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [themeMode, setThemeMode] = useState('light')
+
+  const darkTheme = () => {
+    setThemeMode('dark')
+  }
+
+  const lightTheme = () => {
+    setThemeMode('light')
+  }
+
+  useEffect(() => {
+    const html = document.querySelector('html')
+    if (themeMode === 'dark') {
+      html.classList.add('dark')
+    } else {
+      html.classList.remove('dark')
+    }
+  }, [themeMode])
 
   return (
-    <>
-      
-    </>
+    <ThemeContext.Provider value={{themeMode, darkTheme, lightTheme}}>
+      <div className="flex flex-wrap min-h-screen items-center bg-gray-50 dark:bg-gray-900">
+          <div className="w-full">
+              <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                  <ThemeBtn />
+              </div>
+
+              <div className="w-full max-w-sm mx-auto">
+                  <Card />
+              </div>
+          </div>
+      </div>
+    </ThemeContext.Provider>
   )
 }
 
--- context toggle/src/contexts/theme.js
+++ context toggle/src/contexts/theme.js
@@ -1,14 +1,11 @@
 import { createContext, useContext } from "react";
 
 export const ThemeContext = createContext({
-    themeMode: "light",
-    darkTheme: () => {},
-    lightTheme: () => {},
+  themeMode: "light",
+  darkTheme: () => {},
+  lightTheme: () => {},
 });
 
-export const ThemeProvider = ThemeContext.Provider;
-
-
 export default function useTheme() {
-    return useContext(ThemeContext);
+  return useContext(ThemeContext);
 }
